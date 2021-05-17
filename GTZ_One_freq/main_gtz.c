e4414a70d62d55a8de7e1db749e15ae751e54f4b
Max Trivedi <maxtrivedi@icloud.com>
2021-05-17 11:38:48 +0100

    Updated coefficient for 697Hz

--- GTZ_One_freq/main_gtz.c
+++ GTZ_One_freq/main_gtz.c
@@ -69,7 +69,7 @@ void clk_SWI_Generate_DTMF(UArg arg0)
 	tick = Clock_getTicks();
 
 //	sample = (int) 32768.0*sin(2.0*PI*freq1*TICK_PERIOD*tick) + 32768.0*sin(2.0*PI*freq2*TICK_PERIOD*tick);
-	sample = (int) 32768.0*sin(2.0*PI*freq1*TICK_PERIOD*tick) + 32768.0*sin(2.0*PI*0*TICK_PERIOD*tick);  // WHY WHY why do we use this 32768 as magnitude - as 2^15 value to give a precise 1 second period ( 1Hz) using 15 stage binary counter
+	sample = (int) 32768.0*sin(2.0*PI*freq1*TICK_PERIOD*tick) + 32768.0*sin(2.0*PI*0*TICK_PERIOD*tick);  // Why do we use this 32768 as magnitude - as 2^15 value to give a precise 1 second period ( 1Hz) using 15 stage binary counter
 	sample = sample >>12;
 }
 
@@ -81,7 +81,7 @@ void clk_SWI_Generate_DTMF(UArg arg0)
 void clk_SWI_GTZ_0697Hz(UArg arg0)
 {
    	static int N = 0;
-   	static int Goertzel_Value = 0;
+   	static int Goertzel_Value;
 
    	static short delay;
    	static short delay_1 = 0;
@@ -90,7 +90,8 @@ void clk_SWI_GTZ_0697Hz(UArg arg0)
    	int prod1, prod2, prod3, R_in;
 
    	short input, coef_1;
-   	coef_1 = 0x4A70;
+   	coef_1 = 0x6D02;
+   	coef_1 = coef_1 * 2;
 
    	R_in = sample;// Read the signal in
 
@@ -108,14 +109,15 @@ void clk_SWI_GTZ_0697Hz(UArg arg0)
    		prod2 = (delay_2 * delay_2);
    		prod3 = (delay_1 *  coef_1)>>14;
    		prod3 = prod3 * delay_2;
-   		Goertzel_Value = (prod1 + prod2 -prod3) >> 15;
+   		Goertzel_Value = (prod1 + prod2 - prod3) >> 15;
    		Goertzel_Value <<= 4;   // Scale up value for sensitivity
    		N = 0;
    		delay_1 = delay_2 = 0;
    	}
 
+
    	Goertzel_Value = (((short) R_in) * ((short)Goertzel_Value)) >> 15;
-//   	mcbsp0_write(Goertzel_Value& 0xfffffffe);// Send the signal out 
+//   	mcbsp0_write(Goertzel_Value& 0xfffffffe);// Send the signal out
 
     gtz_out[0] = Goertzel_Value;
     return;
